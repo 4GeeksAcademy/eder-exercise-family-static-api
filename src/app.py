423292ed4370de5b7972a58c2e4d9bf19d6cd6b1
zz-ederfs <161877224+zz-ederfs@users.noreply.github.com>
2024-08-29 03:49:51 +0000

    done

--- src/app.py
+++ src/app.py
@@ -30,13 +30,26 @@ def handle_hello():
 
     # this is how you can use the Family datastructure by calling its methods
     members = jackson_family.get_all_members()
-    response_body = {
-        "hello": "world",
-        "family": members
-    }
+   
+    return jsonify(members), 200
+
+@app.route('/member/<int:id>', methods=['GET'])
+def get_member(id):
+    member = jackson_family.get_member(id)
+    return jsonify(member)
+
+@app.route('/member', methods=['POST'])
+def new_member():
+    body = request.get_json(force=True)
+    jackson_family.add_member(body)
+    return jsonify(body)
 
 
-    return jsonify(response_body), 200
+@app.route('/member/<int:member_id>', methods=['DELETE'])
+def delete_member(member_id):
+    jackson_family.delete_member(member_id)
+    body_response = {"done":True}
+    return jsonify(body_response)
 
 # this only runs if `$ python src/app.py` is executed
 if __name__ == '__main__':
--- src/datastructures.py
+++ src/datastructures.py
@@ -9,25 +9,50 @@ update this file to implement the following already declared methods:
 from random import randint
 
 class FamilyStructure:
-    def __init__(self, last_name):
-        self.last_name = last_name
+    def __init__(self,last_name):
+        self._next_id = 1      
+        self.last_name = last_name   
+        
 
         # example list of members
-        self._members = []
+        self._members = [ {
+    "id": 1,
+    "first_name": "John",
+    "age": 33,
+    "lucky_numbers": [7,13,1]
+},{
+    "id": 2,
+    "first_name": "Jane",
+    "age": 35,
+    "lucky_numbers": [10,14,3]
+},{
+    "id": 3,
+    "first_name": "Jimmy",
+    "age": 5,
+    "lucky_numbers": [1]
+}]
 
     # read-only: Use this method to generate random members ID's when adding members into the list
-    def _generateId(self):
-        return randint(0, 99999999)
+    def _generate_id(self):
+        generated_id =self._next_id
+        self._next_id += 1
+        return generated_id
 
-    def add_member(self, member):
-        # fill this method and update the return
+
+
+    def add_member(self, member):        
+        self._members.append(member)
         pass
 
     def delete_member(self, id):
+        delete_specific_member = next((sub for sub in self._members if sub['id'] == id), None)
+        self._members.remove(delete_specific_member)
         # fill this method and update the return
         pass
 
     def get_member(self, id):
+        specific_member = next((sub for sub in self._members if sub['id'] == id), None)
+        return specific_member
         # fill this method and update the return
         pass
 
